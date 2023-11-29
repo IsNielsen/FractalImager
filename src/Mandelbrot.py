4f4296639fb255dad53464641b6c7f8968f6b638
Isaac Nielsen <a02363142@usu.edu>
2023-11-29 10:07:18 -0700

    changed a little after comparing to files from erik-src

--- src/Mandelbrot.py
+++ src/Mandelbrot.py
@@ -1,8 +1,11 @@
-def mandelbrot_iteration_count(complex, size):
-    z = 0
-
+def mandelbrot_iteration_count(c, size):
+    """
+        Return the iteration count for a point on the complex plane `c`
+        to guess whether it is in the Mandelbrot set (bounded by `end`)
+        """
+    z = complex(0, 0)  # z0
     for i in range(size):
-        z = z * z + complex
-        if abs(z) > 2:
+        z = z * z + c  # Get z1, z2, ...
+        if abs(z) > 2.0:
             return i
-    return i
+    return size - 1
--- src/Phoenix.py
+++ src/Phoenix.py
@@ -1,22 +1,23 @@
-def phoenix_iteration_count(compNum, size):
+def phoenix_iteration_count(z, size):
     """
         Return the iteration count of the current complex number
         within the Phoenix fractal in the palette array
     """
-    # Constants
+    # Julia Constant
     c = complex(0.5667, 0.0)
+
+    # Phoenix Constant
     phoenix = complex(-0.5, 0.0)
 
     # Initialize variables
-    z_flipped = complex(compNum.imag, compNum.real)
+    z = complex(z.imag, z.real)
     z_prev = 0 + 0j
-    z = z_flipped
 
     # Loop over the range of colors in the palette
     for i in range(size):
-        z, z_prev = z * z + c + (phoenix * z_prev), z  # Update Z and prevZ simultaneously
-        # If the absolute value of Z is greater or equal to 2, return the iteration count
+        z_save = z
+        z = z * z + c + (phoenix * z_prev)
         if abs(z) > 2:
-            return i  # The sequence is unbounded
-
-    return size - 1  # Else this is a bounded sequence, return the maximum iteration count
+            return i
+        z_prev = z_save  # Set the prevZ value for the next iteration
+    return size - 1
